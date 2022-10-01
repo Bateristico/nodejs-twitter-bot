64b501d07a519c13df9a30cfe74bb056a706eaab
Camilo Saa <camilo.saa@gmail.com>
2022-10-01 17:11:44 +0200

    set up api credentials

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 const { TwitterApi } = require('twitter-api-v2');
+require('dotenv').config();
+
+const { APP_KEY, APP_SECRET_KEY, ACCESS_TOKEN, ACCESS_TOKEN_SECRET } = process.env;
 
 const client = new TwitterApi({
-  appKey: '',
-  appSecret: '',
-  accessToken: '',
-  accessSecret
+  appKey: APP_KEY,
+  appSecret: APP_SECRET_KEY,
+  accessToken: ACCESS_TOKEN,
+  accessSecret: ACCESS_TOKEN_SECRET
 });
+
+const rwClient = client.readWrite;
+
+module.exports = rwClient;
