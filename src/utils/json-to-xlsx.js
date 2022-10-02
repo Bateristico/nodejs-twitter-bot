0e97323dcf3dbd36b17299f0e831201c6dd2b04b
Camilo Saa <camilo.saa@gmail.com>
2022-10-03 01:28:55 +0200

    add function to write excel file

--- src/utils/json-to-xlsx.js
+++ src/utils/json-to-xlsx.js
@@ -1,16 +1,22 @@
-const json2xlsx = require('node-json-xlsx');
-const fs = require('fs');
+const XLSX = require('xlsx');
 
 async function convertToXlxs(followers) {
-  const xlsx = json2xlsx(followers.data);
-  console.log('xls', xlsx);
-  try {
-    // const file = fs.writeFileSync('data.xlsx', xlsx, 'binary');
-    return file;
-  } catch (error) {
-    console.log('error creating the file', error);
-  }
-  console.log('create file', file);
+  const worksheet = XLSX.utils.json_to_sheet(followers.data);
+  const workbook = XLSX.utils.book_new();
+
+  XLSX.utils.book_append_sheet(workbook, worksheet, 'followers list');
+  // generate buffer
+  XLSX.write(workbook, {
+    bookType: 'xlsx',
+    type: 'buffer'
+  });
+
+  XLSX.write(workbook, {
+    bookType: 'xlsx',
+    type: 'binary'
+  });
+
+  XLSX.writeFile(workbook, 'followers list.xlsx');
 }
 
 module.exports = convertToXlxs;
